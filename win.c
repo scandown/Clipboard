5b1b511e9a4e5f5126c5ff16d9de07a3280a53db
Josh Harris <yesmaileasy@gmail.com>
2026-03-06 17:52:03 +0000

    Updated windows code to get both png and text and store it in a file

--- win.c
+++ win.c
@@ -1,11 +1,45 @@
 #include <windows.h>
 #include <stdio.h>
 
-int paste_clipboard(char **new_data) {
+
+
+int get_png(char **new_data);
+int get_text(char **new_data);
+
+int paste_clipboard(char **new_data, char *file_format_opt) {
+
+	if (strlen(file_format_opt) == strlen("image")) {
+		if (strncmp(file_format_opt, "image", strlen("image")) == 0) {
+			get_png(new_data);
+		}
+	} else if (strlen(file_format_opt) == strlen("text")) {
+		if (strncmp(file_format_opt, "text", strlen("text")) == 0) {
+			get_text(new_data);
+		}
+	} else {
+		fprintf(stderr, "Error: invalid file format given\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	char *data;
+	if (argc > 1) {
+		paste_clipboard(&data, argv[1]);
+	}
+
+	printf("%s\n", data);
+
+	free(data);
+	return 0;
+}
+
+int get_png(char **new_data) {
 	if (!OpenClipboard(NULL)) {
 		return 1;
 	}
-
 	UINT PngFormat = RegisterClipboardFormat("PNG");
 
 	printf("%d\n", PngFormat);
@@ -40,12 +74,33 @@ int paste_clipboard(char **new_data) {
 	return 0;
 }
 
-int main() {
-	char *data;
-	paste_clipboard(&data);
+int get_text(char **new_data) {
+	if (!OpenClipboard(NULL)) {
+		return 1;
+	}
+	HANDLE hdata = GetClipboardData(CF_TEXT);
+	if (hdata == NULL) {
+		CloseClipboard();
+		return 1;
+	}
+	void *data = GlobalLock(hdata);
+	char *mybuffer;
+	if (data) {
+		SIZE_T size = GlobalSize(hdata);
 
-	printf("%s\n", data);
+		mybuffer = malloc(size);
+		memcpy(mybuffer, data, size);
+		FILE *f = fopen("output.txt", "wb");
+		if (f) {
+			fwrite(data, 1, size, f);   // writes 'size' bytes from 'data'
+			fclose(f);
+		} else {
+			perror("Failed to open file");
+		}
+	}
+	GlobalUnlock(hdata);
+	CloseClipboard();
 
-	free(data);
+	*new_data = mybuffer;
 	return 0;
 }
