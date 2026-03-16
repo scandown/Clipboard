5bb0b3c4fc5be9b23814f315eebd0eb4c37dce48
Josh Harris <yesmaileasy@gmail.com>
2026-03-16 14:27:22 +0000

    Changed gitignore and win.c to add a size argument, also added a submodule for wayland clipboard

--- win.c
+++ win.c
@@ -3,18 +3,19 @@
 
 
 
-int get_png(char **new_data);
-int get_text(char **new_data);
+int set_png(char *newdata, size_t newdata_size);
+int get_png(char **new_data, size_t *new_size);
+int get_text(char **new_data, size_t *new_size);
 
-int paste_clipboard(char **new_data, char *file_format_opt) {
+int paste_clipboard(char **new_data, char *file_format_opt, size_t *size) {
 
 	if (strlen(file_format_opt) == strlen("image")) {
 		if (strncmp(file_format_opt, "image", strlen("image")) == 0) {
-			get_png(new_data);
+			get_png(new_data, size);
 		}
 	} else if (strlen(file_format_opt) == strlen("text")) {
 		if (strncmp(file_format_opt, "text", strlen("text")) == 0) {
-			get_text(new_data);
+			get_text(new_data, size);
 		}
 	} else {
 		fprintf(stderr, "Error: invalid file format given\n");
@@ -27,22 +28,81 @@ int paste_clipboard(char **new_data, char *file_format_opt) {
 int main(int argc, char **argv) {
 	char *data;
 	if (argc > 1) {
-		paste_clipboard(&data, argv[1]);
-	}
+		FILE *file = fopen(argv[1], "rb");
+		if (file == NULL) {
+			fprintf(stderr, "Couldn't open file\n");
+			return 1;
+		}
 
-	printf("%s\n", data);
+		char *buffer = NULL; // initialize buffer to NULL
+		int buffer_size = 0;
+		/*Open the file here*/
+		// Read file character by character
+		int c, i = 0;
+		while ((c = fgetc(file)) != EOF) {
+			// If buffer is full, resize it
+			if (i >= buffer_size) {
+				buffer_size += 1000; // increase buffer size by 1000 bytes
+				buffer = realloc(buffer, buffer_size); // resize buffer
+				if (buffer == NULL) {
+					printf("Error: Memory allocation failed.\n");
+					return 1;
+				}
+			}
+			buffer[i] = c;
+			i++;
+		}
+
+		set_png(buffer, buffer_size);
+		free(buffer);
+	}
 
 	free(data);
 	return 0;
 }
 
-int get_png(char **new_data) {
+int set_png(char *newdata, size_t newdata_size) {
+	if (!OpenClipboard(NULL)) {
+		return 1;
+	}
+
+	UINT PngFormat = RegisterClipboardFormat("PNG");
+
+	HANDLE hdata = GlobalAlloc(GMEM_MOVEABLE, newdata_size);
+	if (hdata == NULL) {
+		CloseClipboard();
+		return 1;
+	}
+
+	if (!EmptyClipboard()) {
+		CloseClipboard();
+		return 1;
+	}
+
+
+	void *data = GlobalLock(hdata);
+	if (data == NULL) {
+		CloseClipboard();
+		return 1;
+	}
+
+	memcpy(data, newdata, newdata_size);
+	if (SetClipboardData(PngFormat, hdata) == NULL) {
+		CloseClipboard();
+		GlobalFree(hdata);
+		return 1;
+	}
+
+	GlobalUnlock(hdata);
+	CloseClipboard();
+	return 0;
+}
+
+int get_png(char **new_data, size_t *new_size) {
 	if (!OpenClipboard(NULL)) {
 		return 1;
 	}
 	UINT PngFormat = RegisterClipboardFormat("PNG");
-
-	printf("%d\n", PngFormat);
 
 	HANDLE hdata = GetClipboardData(PngFormat);
 	if (hdata == NULL) {
@@ -65,6 +125,7 @@ int get_png(char **new_data) {
 		} else {
 			perror("Failed to open file");
 		}
+		*new_size = size;
 	}
 	GlobalUnlock(hdata);
 
@@ -74,7 +135,7 @@ int get_png(char **new_data) {
 	return 0;
 }
 
-int get_text(char **new_data) {
+int get_text(char **new_data, size_t *new_size) {
 	if (!OpenClipboard(NULL)) {
 		return 1;
 	}
@@ -97,6 +158,7 @@ int get_text(char **new_data) {
 		} else {
 			perror("Failed to open file");
 		}
+		*new_size = size;
 	}
 	GlobalUnlock(hdata);
 	CloseClipboard();
